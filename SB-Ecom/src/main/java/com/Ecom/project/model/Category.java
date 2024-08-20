ec151d1141b3d57fc2bc5bdc0ebbfc36ce085077
rp22acg <raghu84yadav@gmail.com>
2024-08-20 18:12:55 +0100

    Initial commit

---
+++ SB-Ecom/src/main/java/com/Ecom/project/model/Category.java
@@ -0,0 +1,30 @@
+package com.Ecom.project.model;
+
+public class Category
+{
+    private long categoryID;
+    private String categoryName;
+
+    public Category(long categoryID,String categoryName)
+    {
+        this.categoryID=categoryID;
+        this.categoryName=categoryName;
+    }
+
+    public long getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(long categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
+
