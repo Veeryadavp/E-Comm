c928cb31f24ea66db1eafd667f30e950aa69cc1a
rp22acg <raghu84yadav@gmail.com>
2024-08-23 01:07:26 +0100

    edited

--- SB-Ecom/src/main/java/com/Ecom/project/model/Category.java
+++ SB-Ecom/src/main/java/com/Ecom/project/model/Category.java
@@ -1,7 +1,20 @@
 package com.Ecom.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+//This annotation will convert class in to  a table -
+// and the categoryID attribute should be give @ID annotation -
+// because we should have atleast one unique attribute/column this annotation makes this attribute unique.
+//name attribute of @Entity annotation halps to rename the table name as per our need.
+@Entity(name="Categories")
 public class Category
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryID;
     private String categoryName;
 
